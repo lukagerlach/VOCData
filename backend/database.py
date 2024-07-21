84927a18dc02e8924f5b41302df7028a7c6e32f6
Luka <luka.gerlach@smail.uni-koeln.de>
2024-07-21 16:37:27 +0200

    some test commit

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ import os
 
 from sqlmodel import SQLModel, create_engine
 
-from backend.app.models import Site
+from backend.app.models import Site  # noqa: F401
 
 # retrive the environment variables for the database connection
 ENV_POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
@@ -12,7 +12,10 @@ ENV_POSTGRES_SERVER = os.getenv("POSTGRES_SERVER", "db")
 ENV_POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
 
 # create the database URL
-DATABASE_URL = f"postgresql://{ENV_POSTGRES_USER}:{ENV_POSTGRES_PASSWORD}@{ENV_POSTGRES_SERVER}:{ENV_POSTGRES_PORT}/{ENV_POSTGRES_DB}"
+DATABASE_URL = (
+    f"postgresql://{ENV_POSTGRES_USER}:{ENV_POSTGRES_PASSWORD}"
+    f"@{ENV_POSTGRES_SERVER}:{ENV_POSTGRES_PORT}/{ENV_POSTGRES_DB}"
+)
 
 
 # create the database
