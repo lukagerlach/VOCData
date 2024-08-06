276db8192a9ad19b09f1c42757988a0fe2c46c7e
Luka <luka.gerlach@smail.uni-koeln.de>
2024-08-06 19:48:22 +0200

    feat(7): add project docs

---
+++ backend/docs/source/conf.py
@@ -0,0 +1,38 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("../../.."))
+print(sys.path)
+
+project = "VOCData"
+copyright = "2024, Luka Gerlach"
+author = "Luka Gerlach"
+release = "0.0.1"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",  # include docstrings
+    "pydata_sphinx_theme",  # nice theme
+    "sphinx.ext.viewcode",  # Add links to source code
+]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+nbsphinx_allow_errors = True
+html_theme = "pydata_sphinx_theme"
+html_static_path = ["_static"]
